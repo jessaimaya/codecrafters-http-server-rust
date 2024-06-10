1b55e4cc66233d7079f4ba6c69bbef7ae0cde100
jessai <maya.jessai@gmail.com>
2024-06-10 11:54:57 -0600

    Respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use std::net::TcpListener;
+use std::borrow::BorrowMut;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().borrow_mut() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let buf = "HTTP/1.1 200 OK\r\n\r\n";
+                let _ = stream.write(buf.as_bytes());
             }
             Err(e) => {
                 println!("error: {}", e);
