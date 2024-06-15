0afdf0d9f67b21b1ef4b25b263ec7af2ff9c00e1
jessai <maya.jessai@gmail.com>
2024-06-14 19:27:32 -0600

    return a file

--- src/router.rs
+++ src/router.rs
@@ -1,5 +1,7 @@
+use std::env;
 use crate::httprequest::{self, HttpRequest};
 use crate::httpresponse::HttpResponse;
+use std::collections::HashMap;
 use std::io::Write;
 
 pub struct Router;
@@ -21,6 +23,25 @@ impl Router {
                                 resp.body = Some(header.trim().to_string());
                             }
                             let _ = resp.send_response(stream);
+                        },
+                        "files" => {
+                            let filename = route[2];
+                            let args: Vec<String> = env::args().collect();
+                            let dir = args.last().unwrap().to_string();
+                            let exists = std::path::Path::new(&format!("{}{}", dir, filename)).exists();
+                            if exists {
+                                let b= std::fs::read_to_string(&format!("{}{}", dir, filename)).unwrap();
+                                let b_len = b.len().to_string();
+                                let h = Some(HashMap::from([
+                                    ("Content-Type","application/octet-stream"),
+                                    ("Content-Length", &b_len)
+                                ]));
+                                let resp: HttpResponse = HttpResponse::new("200", h, Some(b));
+                                let _ = resp.send_response(stream);
+                            } else {
+                                let resp: HttpResponse = HttpResponse::new("404", None, None);
+                                let _ = resp.send_response(stream);
+                            }
                         },
                         "echo" => {
                             let resp: HttpResponse = HttpResponse::new(
